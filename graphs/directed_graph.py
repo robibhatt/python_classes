669358a92130e8feaba26c4f575aeec9cae6ee06
Robi Bhattacharjee <robibhatt@gmail.com>
2015-12-22 21:44:23 -0800

    passing first test

--- graphs/directed_graph.py
+++ graphs/directed_graph.py
@@ -1,8 +1,6 @@
 class Directed_Graph:
 
-    inward = 0
-    outward = 1
-    
+
     def __init__(self):
         self.graph_dict = {}
 
@@ -14,19 +12,21 @@ class Directed_Graph:
         in_edges = {}
         self.graph_dict[vertex] = (in_edges, out_edges)
 
-    #overwrites previous edge if exists
+
     def add_edge(self, edge):
         (from_vertex, to_vertex, weight) = edge
-        self.graph_dict[from_vertex][outward][to_vertex] = weight
-        self.graph_dict[to_vertex][inward][from_vertex] = weight
+        self.graph_dict[from_vertex][1][to_vertex] = weight
+        self.graph_dict[to_vertex][0][from_vertex] = weight
+        
 
     def list_vertices(self):
         return self.graph_dict.keys()
+    
 
     def list_edges(self):
         edges = []
         for v in self.graph_dict.keys():
-            edges += [(v, x, self.graph_dict[v][outward][x]) for x in self.graph_dict[v][outward].keys()]
+            edges += [(v, x, self.graph_dict[v][1][x]) for x in self.graph_dict[v][1].keys()]
         return edges
         
         
